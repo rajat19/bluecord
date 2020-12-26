a2e226e754f6422b000d3bbf3c1ce23a25cb7cb8
evilj0ker <rajatsri94@gmail.com>
2020-12-26 15:44:50 +0530

    fixes for heroku

--- db.py
+++ db.py
@@ -4,22 +4,22 @@ import settings as my_settings
 
 
 def create_db_connection():
-    config = {
-        'user': my_settings.DB_USER,
-        'password': my_settings.DB_PASSWORD,
-        'host': my_settings.DB_HOST,
-        'database': my_settings.DB_NAME,
-        'sslmode': 'allow'
-    }
-    # conn = psycopg2.connect(**config)
     conn = psycopg2.connect(my_settings.DATABASE_URL)
     return conn
+
+def create_table():
+    connection = create_db_connection()
+    command = "CREATE TABLE searches (id SERIAL PRIMARY KEY,user_id VARCHAR(255) NOT NULL, keyword VARCHAR(255) NOT NULL, created_at timestamp)"
+    sql_cursor = connection.cursor()
+    sql_cursor.execute(command)
+    connection.commit()
+    connection.close()
 
 
 def post_search_data(user_id, keyword):
     connection = create_db_connection()
     sql_cursor = connection.cursor()
-    sql_cursor.execute("Insert into searches Values('{}', '{}', '{}')".format(
+    sql_cursor.execute("Insert into searches (user_id, keyword, created_at) Values('{}', '{}', '{}')".format(
         user_id, keyword, datetime.now().strftime("%Y-%m-%d %H:%M:%S")))
     connection.commit()
     connection.close()
--- settings.py
+++ settings.py
@@ -1,15 +1,7 @@
 import os
 
-from dotenv import load_dotenv
-
-load_dotenv()
-
 # CREDENTIALS
-BOT_AUTH_TOKEN = os.getenv('BOT_AUTH_TOKEN')
-DB_USER = os.getenv('DB_USER')
-DB_PASSWORD = os.getenv('DB_PASSWORD')
-DB_HOST = os.getenv('DB_HOST')
-DB_NAME = os.getenv('DB_NAME')
-GOOGLE_API_KEY = os.getenv('GOOGLE_SEARCH_API_KEY')
-GOOGLE_CSE_KEY = os.getenv('GOOGLE_CSE_ID')
+BOT_AUTH_TOKEN = os.environ['BOT_AUTH_TOKEN']
+GOOGLE_API_KEY = os.environ['GOOGLE_SEARCH_API_KEY']
+GOOGLE_CSE_KEY = os.environ['GOOGLE_CSE_ID']
 DATABASE_URL=os.environ['DATABASE_URL']
